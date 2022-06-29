5ddf24d92b2d9bcd4d345939198ff8dcf0619952
Kevin Albertson <kevin.albertson@mongodb.com>
2022-06-29 13:17:53 -0400

    use -DMONGOC_TEST_USE_CRYPT_SHARED=FALSE
    It is only required for testing. It requires python3 to build.

--- Formula/mongo-c-driver.rb
+++ Formula/mongo-c-driver.rb
@@ -31,6 +31,7 @@ class MongoCDriver < Formula
     cmake_args = std_cmake_args
     cmake_args << "-DBUILD_VERSION=1.18.0-pre" if build.head?
     cmake_args << "-DCMAKE_INSTALL_RPATH=#{rpath}"
+    cmake_args << "-DMONGOC_TEST_USE_CRYPT_SHARED=FALSE"
     inreplace "src/libmongoc/src/mongoc/mongoc-config.h.in", "@MONGOC_CC@", ENV.cc
     system "cmake", ".", *cmake_args
     system "make", "install"
